14fba6adfb2dc6f5a1c21c8ebf24d8ac7f0ca27f
Petros-13 <petroskalkounos78@gmail.com>
2025-07-18 16:30:23 +0200

    3rd commit

---
+++ Schoolsubjects.py
@@ -0,0 +1,16 @@
+print("How much do you have in each subject?")
+Math=int(input("enter the result you have in math:"))
+Sports=int(input("enter the result you have in sports:"))
+Technologie=int(input("enter the result you have in technologie:"))
+Physics=int(input("enter the result you have in physics:"))
+Chemestry=int(input("enter the result you have in chemestry:"))
+German=int(input("enter the result you have in german:"))
+french=int(input("enter the result you have in french:"))
+English=int(input("enter the result you have in english:"))
+Croatian=int(input("enter the result you have in croatian:"))
+sum=Math+Sports+Technologie+Physics+Chemestry+German+french+English+Croatian
+average=(sum/900)*100
+print("The sum of all subjects is",sum,"and the average of all subjects is",average)
+
+
+
